949878e51dffde3ecbfc34d7c1cc2a22c12d8276
ayoubsofi <ayoub@email.com>
2026-06-23 12:50:30 +0100

    add posts relationship to User model (hasMany)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Post extends Model
-{
-    //
+class Post extends Model {
+    protected $fillable = ['user_id', 'content'];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
