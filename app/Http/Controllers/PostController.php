d3d80ac5e3199958a2e29e674e27f5adbe0d0a7f
ayoubsofi <ayoub@email.com>
2026-06-23 12:52:47 +0100

    implement feed disply

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class PostController extends Controller
 {
     /**
@@ -12,7 +12,7 @@ class PostController extends Controller
     public function index()
     {
         $posts=Post::all();
-        return view("posts.index",compact("posts"));
+        return view("pages.feed",compact("posts"));
     }
 
     /**
