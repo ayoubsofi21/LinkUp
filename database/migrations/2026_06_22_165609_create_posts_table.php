a1008bbba933a81be4991a5c1fb63bfa5b3bb03e
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-06-25 22:18:20 +0100

    update posts table on using all column i need to display data of user

--- database/migrations/2026_06_22_165609_create_posts_table.php
+++ database/migrations/2026_06_22_165609_create_posts_table.php
@@ -11,12 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Schema::create('posts', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->string('title');
-        //     $table->text('body');
-        //     $table->timestamps();
-        // });
         Schema::create('posts',function(Blueprint $table){
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
