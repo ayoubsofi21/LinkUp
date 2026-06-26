18872c5ad2a16bb85c3ae14b3d166cd59057ae50
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-06-26 10:49:57 +0100

    make new branch for training

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>new Project</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>All Posts</h1>
-    @foreach($posts as $post){
-        <h2>{{$post->title}}</h2>
-        <p>{{$post->body}}</p>
-    }
+    <h1>hello</h1>
 </body>
 </html>
