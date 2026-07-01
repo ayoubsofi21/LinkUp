7060af7687c9227f7161407227feb964bb500b76
ayoubsofi <ayoub@email.com>
2026-07-01 11:01:20 +0100

    add UserFactory and configure model for seeding

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable {
+        use HasFactory, Notifiable;
     protected $fillable = [
         'name', 'email', 'password', 'headline', 'company', 'image_url'
     ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -22,16 +23,26 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+     public function definition(): array
+        {
+            return [
+                'name' => fake()->name(),
+
+                'email' => fake()->unique()->safeEmail(),
+
+                'email_verified_at' => now(),
+
+                'password' => bcrypt('password'),
+
+                'headline' => fake()->jobTitle(),
+
+                'company' => fake()->company(),
+
+                'image_url' => 'https://picsum.photos/300?random=' . fake()->unique()->numberBetween(1, 10000),
+
+                'remember_token' => fake()->regexify('[A-Za-z0-9]{10}')
+            ];
+        }
 
     /**
      * Indicate that the model's email address should be unverified.
